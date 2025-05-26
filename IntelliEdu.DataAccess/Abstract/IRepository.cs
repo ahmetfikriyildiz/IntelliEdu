03f7b518f1671ca1854e721ab46f8240e02bd997
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2025-05-26 19:27:21 +0300

    repositories

---
+++ IntelliEdu.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliEdu.DataAccess.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> GetList();
+        T GetById(int id);
+        T GetByFilter(Expression<Func<T, bool>> predicate);
+        int FilteredCount(Expression<Func<T, bool>> predicate);
+        List<T> GetFilteredList(Expression<Func<T, bool>> predicate);
+        void Delete(int id);
+        void Create(T entity);
+        void Update(T entity);
+        int Count();
+    }
+}
---
+++ IntelliEdu.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using IntelliEdu.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliEdu.DataAccess.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly IRepository<T> _repository;
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FilteredCount(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByFilter(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetFilteredList(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
